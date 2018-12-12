2bfa34b54059f3655f71e3da2fe867be10976dc1
BEHERETCHE Sebastien <sbeheret@student.42.fr>
2018-12-12 16:07:42 +0100

    lol

--- virtual_machine/src/instruction2.c
+++ virtual_machine/src/instruction2.c
@@ -6,74 +6,61 @@
 /*   By: rfibigr <rfibigr@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 18:43:38 by rfibigr           #+#    #+#             */
-/*   Updated: 2018/12/12 13:16:13 by rfibigr          ###   ########.fr       */
+/*   Updated: 2018/12/12 16:06:56 by sbeheret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-void	ft_and(t_vm *vm, t_processus *processus)
+void	ft_and(t_vm *vm, t_processus *pcs)
 {
-	// ft_printf("~~~AND~~~\n");
-	int			error;
+	int			value1;
+	int			value2;
 	t_action	*action;
 
-	action = &(processus->action);
-	error = 0;
-	if (action->type[0] == REG)
-		action->args[ARG1] = processus->reg[action->args[0]];
-	else if (action->type[0] == IND)
-		action->args[ARG1] = ft_get_ind(vm, processus, ARG1);
-	if (action->type[1] == REG)
-		action->args[ARG1] = processus->reg[action->args[1]];
-	else if (action->type[1] == IND)
-		action->args[ARG2] = ft_get_ind(vm, processus, ARG2);
-	processus->reg[action->args[2]] = action->args[0] & action->args[1];
-	processus->carry = 1;
+	action = &(pcs->action);
+	value1 = get_content_value(vm->ram, pcs, action->type[0], action->args[0]);
+	value2 = get_content_value(vm->ram, pcs, action->type[1], action->args[1]);
+	if ((pcs->reg[action->args[2]] = value1 & value2) == 0)
+		pcs->carry = 1;
+	else
+		pcs->carry = 0;
 	if ((*vm).verbose)
-		ft_print_add(processus, action->args[0], action->args[1], action->args[2]);
+		ft_print_add(pcs, action->args[0], action->args[1], action->args[2]);
 }
 
-void	ft_or(t_vm *vm, t_processus *processus)
+void	ft_or(t_vm *vm, t_processus *pcs)
 {
-	int			error;
+	int			value1;
+	int			value2;
 	t_action	*action;
 
-	action = &(processus->action);
-	error = 0;
-	if (action->type[0] == REG)
-		action->args[ARG1] = processus->reg[action->args[0]];
-	else if (action->type[0] == IND)
-		action->args[ARG1] = ft_get_ind(vm, processus, ARG1);
-	if (action->type[1] == REG)
-		action->args[ARG1] = processus->reg[action->args[1]];
-	else if (action->type[1] == IND)
-		action->args[ARG2] = ft_get_ind(vm, processus, ARG2);
-	processus->reg[action->args[2]] = action->args[0] | action->args[1];
-	processus->carry = 1;
+	action = &(pcs->action);
+	value1 = get_content_value(vm->ram, pcs, action->type[0], action->args[0]);
+	value2 = get_content_value(vm->ram, pcs, action->type[1], action->args[1]);
+	if ((pcs->reg[action->args[2]] = value1 | value2) == 0)
+		pcs->carry = 1;
+	else
+		pcs->carry = 0;
 	if ((*vm).verbose)
-		ft_print_or(processus, action->args[0], action->args[1], action->args[2]);
+		ft_print_or(pcs, action->args[0], action->args[1], action->args[2]);
 }
 
-void	ft_xor(t_vm *vm, t_processus *processus)
+void	ft_xor(t_vm *vm, t_processus *pcs)
 {
-	int			error;
+	int			value1;
+	int			value2;
 	t_action	*action;
 
-	action = &(processus->action);
-	error = 0;
-	if (action->type[0] == REG)
-		action->args[ARG1] = processus->reg[action->args[0]];
-	else if (action->type[0] == IND)
-		action->args[ARG1] = ft_get_ind(vm, processus, ARG1);
-	if (action->type[1] == REG)
-		action->args[ARG1] = processus->reg[action->args[1]];
-	else if (action->type[1] == IND)
-		action->args[ARG2] = ft_get_ind(vm, processus, ARG2);
-	processus->reg[action->args[2]] = action->args[0] ^ action->args[1];
-	processus->carry = 1;
+	action = &(pcs->action);
+	value1 = get_content_value(vm->ram, pcs, action->type[0], action->args[0]);
+	value2 = get_content_value(vm->ram, pcs, action->type[1], action->args[1]);
+	if ((pcs->reg[action->args[2]] = value1 ^ value2) == 0)
+		pcs->carry = 1;
+	else
+		pcs->carry = 0;
 	if ((*vm).verbose)
-		ft_print_xor(processus, action->args[0], action->args[1], action->args[2]);
+		ft_print_xor(pcs, action->args[0], action->args[1], action->args[2]);
 }
 
 void	ft_zjump(t_vm *vm, t_processus *processus)
@@ -93,25 +80,23 @@ void	ft_zjump(t_vm *vm, t_processus *processus)
 }
 
 
-void	ft_ldi(t_vm *vm, t_processus *processus)
+void	ft_ldi(t_vm *vm, t_processus *pcs)
 {
 	//DIRECT 2 bytes
-	// ft_printf("~~~LD~~~\n");
-	int			address;
 	t_action	*action;
+	int			value1;
+	int			value2;
+	int			addrs;
 
-	action = &(processus->action);
-	if (action->type[0] == REG)
-		action->args[ARG1] = processus->reg[action->args[0]];
-	else if (action->type[0] == IND)
-		action->args[ARG1] = ft_get_ind(vm, processus, ARG1);
-	if (action->type[1] == REG)
-		action->args[ARG1] = processus->reg[action->args[1]];
-	else if (action->type[1] == IND)
-		action->args[ARG2] = ft_get_ind(vm, processus, ARG2);
-	address = circular(action->pc + (action->args[0] + action->args[1]));
-	processus->reg[action->args[2]] = ft_octet_to_int2((*vm).ram, REG_SIZE, address);
-	processus->carry = 1;
+	action = &(pcs->action);
+	value1 = get_content_value(vm->ram, pcs, action->type[0], action->args[0]);
+	value2 = get_content_value(vm->ram, pcs, action->type[1], action->args[2]);
+	addrs = circular(pcs->action.pc + ((value1 + value2) % IDX_MOD));
+	pcs->reg[pcs->action.args[2]] = ft_octet_to_int2(vm->ram, REG_SIZE, addrs);
+	if (pcs->reg[pcs->action.args[2]] == 0)
+		pcs->carry = 1;
+	else
+		pcs->carry = 0;
 	if ((*vm).verbose)
-		ft_print_ldi(processus, action->args[0], action->args[1], action->args[2]);
+		ft_print_ldi(pcs, action->args[0], action->args[1], action->args[2]);
 }
